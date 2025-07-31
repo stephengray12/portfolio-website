1ccec5a21be387ab6974d81e5f6f470cf9d12270
Stephen Gray <97068845+stephengray12@users.noreply.github.com>
2025-07-31 18:51:13 -0500

    Added more items to navbar

--- src/app/taskbar.js
+++ src/app/taskbar.js
@@ -7,7 +7,11 @@ export default function Taskbar() {
   const [open, setOpen] = useState(false);
 
   const projects = [
-    { name: "Portfolio", href: "#projects" },
+    { name: "Home", href: "/" },
+    { name: "About", href: "#About" },
+    { name: "Contact", href: "#Contact" },
+    { name: "Projects", href: "#Projects" },
+    { name: "Resume", href: "/StephenGrayResume.pdf" },
   ];
 
   return (
@@ -18,9 +22,9 @@ export default function Taskbar() {
         </button>
 
         {/* Desktop Menu */}
-        <nav className="hidden md:flex gap-6">
+        <nav className="hidden md:flex gap-6 ">
           {projects.map((project) => (
-            <Link key={project.name} href={project.href} className="hover:text-blue-400 transition-colors duration-300">
+            <Link key={project.name} href={project.href} className="hover:text-blue-400 transition-colors duration-500">
               {project.name}
             </Link>
           ))}
@@ -30,7 +34,7 @@ export default function Taskbar() {
       {/* Mobile Menu */}
       {open && (
         <div className="md:hidden mt-4 px-4">
-          <nav className="flex flex-col gap-4 bg-gray-800 p-4 rounded-lg">
+          <nav className="flex flex-col gap-4 bg-black p-4 rounded-lg">
             {projects.map((project) => (
               <Link
                 key={project.name}
